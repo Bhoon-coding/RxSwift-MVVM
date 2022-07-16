e2048fd6cfbe126d1843061653a1d3da2cd5bad0
bhoon-coding <bhooncoding@gmail.com>
2022-07-17 00:44:33 +0900

    MenuListViewModel: PublishSubject -> BehaviorSubject로 변경 (데이터를 받아오고 싶은 시점 변경)

--- Example/season2/step3+Empty/RxSwift+MVVM/Pages/MenuList/MenuViewController.swift
+++ Example/season2/step3+Empty/RxSwift+MVVM/Pages/MenuList/MenuViewController.swift
@@ -11,28 +11,38 @@ import RxSwift
 import RxCocoa
 
 class MenuViewController: UIViewController {
-    // MARK: - Life Cycle
     
+    let cellId = "MenuItemTableViewCell"
     let viewModel = MenuListViewModel()
     var disposeBag = DisposeBag()
+    
+    // MARK: - Life Cycle
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // 26번째 .bind와 31번째 .subscribe는 같은동작
+        tableView.dataSource = nil
+        
+        viewModel.menuObservable
+            .observeOn(MainScheduler.instance)
+            .bind(to: tableView.rx.items(cellIdentifier: cellId, cellType: MenuItemTableViewCell.self)) { index, item, cell in
+                cell.title.text = item.name
+                cell.price.text = "\(item.price)"
+                cell.count.text = "\(item.count)"
+            }
+            .disposed(by: disposeBag)
         
         viewModel.itemsCount
             .map { "\($0)"}
+            .observeOn(MainScheduler.instance)
             .bind(to: itemCountLabel.rx.text)
             .disposed(by: disposeBag)
         
         viewModel.totalPrice
             .map { $0.currencyKR() }
-        // bind를 사용하면 [weak self] 처럼 순환참조 처리 안해줘도 됨
-            .bind(to: totalPrice.rx.text) // 아래와 같은뜻
-//            .subscribe(onNext: { [weak self] _ in
-//                self?.totalPrice.text = $0
-//            })
+            .observeOn(MainScheduler.instance)
+            .bind(to: totalPrice.rx.text)
             .disposed(by: disposeBag)
     }
 
@@ -62,7 +72,7 @@ class MenuViewController: UIViewController {
 
     @IBAction func onOrder(_ sender: UIButton) {
         // TODO: no selection
-        viewModel.totalPrice.onNext(100)
+//        viewModel.totalPrice.onNext(100)
         // showAlert("Order Fail", "No Orders")
 //        performSegue(withIdentifier: "OrderViewController", sender: nil)
     }
@@ -73,19 +83,19 @@ class MenuViewController: UIViewController {
     }
 }
 
-extension MenuViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel.menus.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MenuItemTableViewCell") as! MenuItemTableViewCell
-
-        let menu = viewModel.menus[indexPath.row]
-        cell.title.text = menu.name
-        cell.price.text = "\(menu.price)"
-        cell.count.text = "\(menu.count)"
-
-        return cell
-    }
-}
+//extension MenuViewController: UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return viewModel.menus.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "MenuItemTableViewCell") as! MenuItemTableViewCell
+//
+//        let menu = viewModel.menus[indexPath.row]
+//        cell.title.text = menu.name
+//        cell.price.text = "\(menu.price)"
+//        cell.count.text = "\(menu.count)"
+//
+//        return cell
+//    }
+//}
