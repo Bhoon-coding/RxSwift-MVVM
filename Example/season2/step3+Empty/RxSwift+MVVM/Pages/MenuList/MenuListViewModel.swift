384bbbc5395c030623049ae6a8dce061ae256603
bhoon-coding <bhooncoding@gmail.com>
2022-07-16 23:55:55 +0900

    MenuListViewModel 생성 및 리팩토링

--- Example/season2/step3+Empty/RxSwift+MVVM/Pages/MenuList/MenuListViewModel.swift
+++ Example/season2/step3+Empty/RxSwift+MVVM/Pages/MenuList/MenuListViewModel.swift
@@ -11,15 +11,28 @@ import RxSwift
 
 class MenuListViewModel {
     
-    var menus: [Menu] = [
-        Menu(name: "튀김", price: 500, count: 0),
-        Menu(name: "떡볶이", price: 2000, count: 0),
-        Menu(name: "순대", price: 1000, count: 0),
-        Menu(name: "오뎅", price: 500, count: 0)
-    ]
+    var menuObservable = PublishSubject<[Menu]>()
     
-    var itemsCount: Int = 5
-    var totalPrice: PublishSubject<Int> = PublishSubject()
+    lazy var itemsCount = menuObservable.map {
+        $0.map { $0.count }.reduce(0, +)
+    }
+    lazy var totalPrice = menuObservable.map {
+        $0.map { $0.price * $0.count }.reduce(0, +)
+    }
+    
+    init() {
+        let menus: [Menu] = [
+            Menu(name: "튀김", price: 500, count: 0),
+            Menu(name: "떡볶이", price: 2000, count: 0),
+            Menu(name: "순대", price: 1000, count: 0),
+            Menu(name: "오뎅", price: 500, count: 0)
+        ]
+        menuObservable.onNext(menus)
+    }
+    
+    
+    
+    
     
     // Subject -> 외부에서 값을 통제
 }
